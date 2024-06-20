bc721d23aebccd5e8562844a2d6157ba2d9f0fff
@Nitu2747 <nitujadhav501@gmail.com>
2024-06-20 09:04:00 +0530

    Commit for added Logger information to Service layer.

--- src/main/java/com/crud/service/impl/UserServiceImpl.java
+++ src/main/java/com/crud/service/impl/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.crud.service.impl;
 import com.crud.model.User;
 import com.crud.repositoy.UserRepository;
 import com.crud.service.UserServiceI;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +12,20 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserServiceI {
+
+   // Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public User createUser(User user) {
+        log.info("Initiating the dao call to save user");
         User saveUser = userRepository.save(user);//cntrl+alt+v
+        log.info("Request to dao got completed to save data");
         return saveUser;
 
     }
@@ -35,9 +45,10 @@ public class UserServiceImpl implements UserServiceI {
 
     @Override
     public User getUserById(Long userId) {
-
+      log.info("Initiate dao call to get single user data with this userId{}  ",userId);
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("Resource Not Found On Server"));
+        log.info("Completed dao call to get single user data with this userId{}  ",userId);
         return user;
     }
 
